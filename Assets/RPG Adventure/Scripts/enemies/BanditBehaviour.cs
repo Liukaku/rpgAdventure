bc6a646315fa3a0caf5c72467e564b038e8fe7f6
Liukaku <46352428+Liukaku@users.noreply.github.com>
2023-10-20 20:26:24 +0100

    add assets and update bandit behaviour

--- Assets/RPG Adventure/Scripts/enemies/BanditBehaviour.cs
+++ Assets/RPG Adventure/Scripts/enemies/BanditBehaviour.cs
@@ -1,19 +1,31 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RpgAdventure { 
     public class BanditBehaviour : MonoBehaviour
     {
 
         public float detectionRange = 10.0f;
-        private void Start()
+        public float detectionAngle = 90.0f;
+
+        private PlayerController m_Player;
+        private NavMeshAgent m_NavMeshAgent;
+
+        private void Awake()
         {
-            Debug.Log(PlayerController.Instance);
+            m_NavMeshAgent = GetComponent<NavMeshAgent>();
         }
 
         private void Update()
         {
-            DetectPlayer();
+            m_Player = DetectPlayer();
+
+            if (!m_Player) { return; }
+
+           Vector3 targetPosition = m_Player.transform.position;
+
+           m_NavMeshAgent.SetDestination(targetPosition);
         }
 
         private PlayerController DetectPlayer()
@@ -26,14 +38,40 @@ namespace RpgAdventure {
             Vector3 disToPlayer = PlayerController.Instance.transform.position - enemyPosition;
             disToPlayer.y = 0; // to make debugging easier 
 
+            // check if the player is within a general range
             if(disToPlayer.magnitude <= detectionRange)
             {
-                Debug.Log("detecting the player ");
-            } else
-            {
-                Debug.Log("where are you");
-            }
+                // check if the player is within the angle specified in decectionAngle
+                if(Vector3.Dot(disToPlayer.normalized, transform.forward) > 
+                    Mathf.Cos(detectionAngle * 0.5f * Mathf.Deg2Rad ))
+                {
+                    Debug.Log("player detected");
+                    return PlayerController.Instance;
+                }
+            } 
             return null;
         }
+
+        # if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Color c = new Color(0.0f, 0.0f, 1.0f, 0.5f);
+            UnityEditor.Handles.color = c;
+
+            Vector3 rotatedForward = Quaternion.Euler(
+                    0,
+                    -detectionAngle * 0.5f,
+                    0)
+                * transform.forward;
+
+            UnityEditor.Handles.DrawSolidArc(
+                transform.position, 
+                Vector3.up, 
+                rotatedForward,
+                detectionAngle, 
+                detectionRange
+                );
+        }
+        #endif
     }
 }
