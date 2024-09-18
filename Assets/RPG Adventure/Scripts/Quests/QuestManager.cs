d32aca161107256aef545819e1278e5019a672b0
liukaku <deazee@dz>
2024-09-18 08:46:47 +0100

    set up loading quests from json file

--- Assets/RPG Adventure/Scripts/Quests/QuestManager.cs
+++ Assets/RPG Adventure/Scripts/Quests/QuestManager.cs
@@ -1,11 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace RpgAdventure
 {
     public class QuestManager : MonoBehaviour
     {
-        public List<Quest> quests;
+        public class JsonHelper
+        {
+            private class Wrapper<T>
+            {
+                public T[] array;
+            }
+
+            public static T[] GetJsonFromArray<T>(string json)
+            {
+                string newJson = "{\"array\": " + json + "}";
+                Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+                return wrapper.array;
+            }
+        }
+
+        public Quest[] quests;
+
+        public void Awake()
+        {
+            LoadQuestsFromDB();
+        }
+
+        private void LoadQuestsFromDB()
+        {
+            using (StreamReader r = new StreamReader("Assets/RPG Adventure/DB/QuestDB.json"))
+            {
+                string json = r.ReadToEnd();
+                var loadedQuests = JsonHelper.GetJsonFromArray<Quest>(json);
+                quests = new Quest[loadedQuests.Length];
+                quests = loadedQuests;
+                Debug.Log(quests);
+            }
+        }
+
     }
 }
