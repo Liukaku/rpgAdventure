f325a971f9acbb06af3c6f2859b82529f9f5e014
liukaku <liukakuworks@protonmail.ch>
2024-09-30 16:08:58 +0100

    set up dialog options

--- Assets/RPG Adventure/Scripts/Dialog/DialogManager.cs
+++ Assets/RPG Adventure/Scripts/Dialog/DialogManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,10 +8,27 @@ namespace RpgAdventure
 {
     public class DialogManager : MonoBehaviour
     {
+        public float timeToShowOptions = 2.0f;
         public GameObject dialogUI;
         public TextMeshProUGUI dialogHeaderText;
+        public TextMeshProUGUI dialogAnswerText;
+        public GameObject dialogOptionList;
+        public Button dialogOptionPrefab;
 
-        private bool m_hasActiveDialog;
+        private PlayerInput m_Player;
+        private QuestGiver m_Npc;
+        private Dialog m_ActiveDialog;
+        private float m_OptionTopPosition;
+        private float m_TimerToShowOptions;
+
+        const float c_DistanceBetweenOptions = 32.0f;
+
+        public bool HasActiveDialog { get { return m_ActiveDialog != null; } }
+
+        private void Start()
+        {
+            m_Player = PlayerInput.Instance;
+        }
 
         private void Awake()
         {
@@ -19,25 +37,109 @@ namespace RpgAdventure
 
         void Update()
         {
-            if (!m_hasActiveDialog && PlayerInput.Instance != null && PlayerInput.Instance.interactTarget != null)
+            if (!HasActiveDialog && m_Player != null && m_Player.interactTarget != null)
             {
-                foreach (Collider collider in PlayerInput.Instance.interactTarget)
+                foreach (Collider collider in m_Player.interactTarget)
                 {
                     if (collider.CompareTag("QuestGiver"))
                     {
+                        m_Npc = collider.gameObject.GetComponent<QuestGiver>();
                         StartDialog();
                     }
                 }
 
             }
+
+            if (HasActiveDialog && Vector3.Distance(m_Player.transform.position, m_Npc.transform.position) > m_Player.distanceToInteract) 
+            {
+                StopDialog();
+            }
+
+            if(m_TimerToShowOptions > 0)
+            {
+                m_TimerToShowOptions += Time.deltaTime;
+            }
+
+            if(m_TimerToShowOptions >= timeToShowOptions)
+            {
+                m_TimerToShowOptions = 0;
+                DisplayDialogOptioins();
+            }
         }
 
         private void StartDialog()
         {
-            m_hasActiveDialog = true;
+            m_ActiveDialog = m_Npc.dialog;
             Debug.Log("starting dialog");
+            dialogHeaderText.text = m_Npc.name;
             dialogUI.SetActive(true);
-            dialogHeaderText.text = "Hello from code";
+
+            ClearDialogOptions();
+            DisplayAnswerText(m_ActiveDialog.welcomeText);
+            TriggerDialogOptions();
+        }
+
+        private void DisplayAnswerText(string answerText)
+        {
+            dialogAnswerText.gameObject.SetActive(true);
+            dialogAnswerText.text = answerText;
+        }
+
+        private void DisplayDialogOptioins()
+        {
+            HideAnswerText();
+            CreateDialogMenu();
+        }
+
+        private void TriggerDialogOptions()
+        {
+            m_TimerToShowOptions = 0.001f;
+        }
+
+        private void HideAnswerText()
+        {
+            dialogAnswerText.gameObject.SetActive(false);
+        }
+
+        private void CreateDialogMenu()
+        {
+            m_OptionTopPosition = 0;
+            var queries = Array.FindAll(m_ActiveDialog.queries, query => !query.isAsked);
+
+            foreach (var query in queries) 
+            {
+                m_OptionTopPosition += c_DistanceBetweenOptions;
+                Debug.Log(query.text);
+                Button DialogOption = CreateDialogOption(query.text);
+
+            }
+        }
+
+        private Button CreateDialogOption(string dialogOptionText)
+        {
+            Button buttonInstance = Instantiate(dialogOptionPrefab, dialogOptionList.transform);
+            buttonInstance.GetComponentInChildren<TextMeshProUGUI>().text = dialogOptionText;
+
+            RectTransform rt = buttonInstance.GetComponent<RectTransform>();
+            rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, m_OptionTopPosition, rt.rect.height);
+
+            return buttonInstance;
+        }
+
+        private void StopDialog()
+        {
+            m_Npc = null;
+            m_ActiveDialog = null;
+            m_TimerToShowOptions = 0;
+            dialogUI.SetActive(false);
+        }
+
+        private void ClearDialogOptions()
+        {
+            foreach(Transform child in dialogOptionList.transform)
+            {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
